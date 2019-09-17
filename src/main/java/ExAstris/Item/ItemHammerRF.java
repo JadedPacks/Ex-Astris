99908b09ae09dfd4dd834c502b612f7d5be2239d
Kris <cblair91@gmail.com>
2019-09-18 00:45:43 +0100

    Fix https://github.com/JadedPacks/Agrarian-Skies-2/issues/49

--- src/main/java/ExAstris/Item/ItemHammerRF.java
+++ src/main/java/ExAstris/Item/ItemHammerRF.java
@@ -4,100 +4,45 @@ import ExAstris.Data.ItemData;
 import ExAstris.Data.ModData;
 import cofh.redstonearsenal.item.RAItems;
 import cofh.redstonearsenal.item.tool.ItemToolRF;
+import exnihilo.items.hammers.IHammer;
 import exnihilo.registries.HammerRegistry;
-import exnihilo.registries.helpers.Smashable;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
-import net.minecraft.world.World;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
-public class ItemHammerRF extends ItemToolRF {
+public class ItemHammerRF extends ItemToolRF implements IHammer {
 	IIcon activeIcon;
 	IIcon drainedIcon;
-	//public static Set blocksEffectiveAgainst = Sets.newHashSet(new Block[]{});
 
 	public ItemHammerRF() {
 		super(RAItems.TOOL_MATERIAL_FLUX);
 		this.energyPerUseCharged = 1600;
 		setCreativeTab(ExAstris.ExAstris.ExAstrisTab);
+		setHarvestLevel("pickaxe", 3);
+	}
+
+	@Override
+	public boolean isHammer(ItemStack stack) {
+		return true;
 	}
 
 	@Override
 	public boolean canItemHarvestBlock(Block block) {
-		return HammerRegistry.registered(block, 0);
+		return HammerRegistry.registered(new ItemStack(block));
 	}
 
 	@Override
 	public float getDigSpeed(ItemStack item, Block block, int meta) {
-		if(isEmpowered(item)) {
-			return efficiencyOnProperMaterial * 6.0f;
-		} else {
-			if(getEnergyStored(item) >= getEnergyPerUse(item)) {
+		if (HammerRegistry.registered(new ItemStack(block, 1, meta)) && block.getHarvestLevel(meta) <= this.toolMaterial.getHarvestLevel()) {
+			if(isEmpowered(item)) {
+				return efficiencyOnProperMaterial * 6.0f;
+			} else if(getEnergyStored(item) >= getEnergyPerUse(item)) {
 				return efficiencyOnProperMaterial * 1.0f;
 			}
-
 		}
 		return 0.5f;
-	}
-
-	@Override
-	public boolean onBlockStartBreak(ItemStack item, int X, int Y, int Z, EntityPlayer player) {
-		World world = player.worldObj;
-		Block block = world.getBlock(X, Y, Z);
-		int blockMeta = world.getBlockMetadata(X, Y, Z);
-		int fortune = EnchantmentHelper.getFortuneModifier(player);
-		boolean valid = false;
-
-		ArrayList<Smashable> rewards = HammerRegistry.getRewards(block, blockMeta);
-
-		if(getEnergyStored(item) < getEnergyPerUse(item)) {
-			return false;
-		}
-		if(rewards.size() > 0 && block.getHarvestLevel(blockMeta) <= this.toolMaterial.getHarvestLevel()) {
-			Iterator<Smashable> it = rewards.iterator();
-			while(it.hasNext()) {
-				Smashable reward = it.next();
-
-				if(!world.isRemote && world.rand.nextFloat() <= reward.chance + (reward.luckMultiplier * fortune)) {
-					EntityItem entityitem = new EntityItem(world, (double) X + 0.5D, (double) Y + 0.5D, (double) Z + 0.5D, new ItemStack(reward.item, 1, reward.meta));
-
-					double f3 = 0.05F;
-					entityitem.motionX = world.rand.nextGaussian() * f3;
-					entityitem.motionY = (0.2d);
-					entityitem.motionZ = world.rand.nextGaussian() * f3;
-
-					world.spawnEntityInWorld(entityitem);
-
-				}
-
-				valid = true;
-			}
-			if(valid) {
-				useEnergy(item, false);
-			}
-		} else {
-			if(block.getMaterial().isToolNotRequired() || block.getHarvestTool(blockMeta) == null) {
-				return false;
-			}
-		}
-
-		if(item.stackSize == 0) {
-			player.destroyCurrentEquippedItem();
-		}
-
-		if(!world.isRemote) {
-			//world.func_147480_a(X, Y, Z, false);
-		}
-
-		return valid;
 	}
 
 	@Override
@@ -122,7 +67,7 @@ public class ItemHammerRF extends ItemToolRF {
 		return getEnergyStored(paramItemStack) <= 0 ? this.drainedIcon : isEmpowered(paramItemStack) ? this.activeIcon : this.itemIcon;
 	}
 
-	public EnumRarity getRarity() {
+	public EnumRarity getRarity(ItemStack itemStack) {
 		return EnumRarity.uncommon;
 	}
 
