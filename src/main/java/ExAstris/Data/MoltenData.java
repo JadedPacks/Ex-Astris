bf285e649c862eea61052588b0ad6e159b449ec1
Kris <cblair91@gmail.com>
2019-09-18 01:09:43 +0100

    Fix ore multiplication issues

--- src/main/java/ExAstris/Data/MoltenData.java
+++ src/main/java/ExAstris/Data/MoltenData.java
@@ -3,6 +3,6 @@ package ExAstris.Data;
 import tconstruct.TConstruct;
 
 public class MoltenData {
-	public static final int ingotCostSmeltery = TConstruct.ingotLiquidValue * 2;
-	public static final int ingotCostHighoven = TConstruct.ingotLiquidValue * 3;
+	public static final int ingotCostSmeltery = TConstruct.ingotLiquidValue;
+	public static final int ingotCostHighoven = TConstruct.ingotLiquidValue;
 }
